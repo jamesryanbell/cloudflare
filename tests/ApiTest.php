612b61cdf24c9153a9952de782b88d1d9dc46c58
James Bell <james@james-bell.co.uk>
2014-08-10 17:48:45 +0100

    Added unit tests for the API class

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,13 +1,50 @@
 <?php
- 
+
 use JamesRyanBell\Cloudflare;
- 
-class CloudflareTest extends PHPUnit_Framework_TestCase {
- 
-  public function testApiClassFound()
-  {
-    $api = new Cloudflare\Api;
-    $this->assertTrue((bool)$api);
-  }
- 
+
+class ApiTest extends PHPUnit_Framework_TestCase {
+
+	public function testApiClassFound()
+	{
+		$api = new Cloudflare\Api;
+		$this->assertTrue((bool)$api);
+	}
+
+	public function testApiInitialised()
+	{
+		$api = new Cloudflare\Api('email@example.com', 'Auth Key');
+		$this->assertEquals('email@example.com', $api->email);
+		$this->assertEquals('Auth Key', $api->auth_key);
+	}
+
+	public function testApiInitialisedFromPreviousObject()
+	{
+		$client = new Cloudflare\Api('email@example.com', 'Auth Key');
+		$api = new Cloudflare\Api($client);
+		$this->assertEquals('email@example.com', $api->email);
+		$this->assertEquals('Auth Key', $api->auth_key);
+	}
+
+	public function testSetAuthKey()
+	{
+		$api = new Cloudflare\Api;
+		$api->setAuthKey('Auth Key');
+		$this->assertEquals('Auth Key', $api->auth_key);
+	}
+
+	public function testSetEmail()
+	{
+		$api = new Cloudflare\Api;
+		$api->setEmail('email@example.com');
+		$this->assertEquals('email@example.com', $api->email);
+	}
+
+	public function testSetCurlOption()
+	{
+		$api = new Cloudflare\Api;
+		$api->setCurlOption(array(CURLOPT_TIMEOUT => 5));
+		$this->assertArrayHasKey(CURLOPT_TIMEOUT, $api->curl_options);
+		$this->assertEquals(5, $api->curl_options[CURLOPT_TIMEOUT]);
+	}
+
 }
