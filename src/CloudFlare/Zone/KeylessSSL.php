ea71ed522bb52dfbd4650f7a8838106753afe8c5
James Bell <james@james-bell.co.uk>
2016-04-24 08:52:44 +0100

    Removed duplicate line in the doc block

--- src/CloudFlare/Zone/KeylessSSL.php
+++ src/CloudFlare/Zone/KeylessSSL.php
@@ -25,7 +25,6 @@ class KeylessSSL extends Api
     /**
      * Create a Keyless SSL configuration (permission needed: #ssl:edit)
      * @param string      $zone_identifier API item identifier tag
-     * @param string      $zone_identifier
      * @param string      $host            The keyless SSL host
      * @param int         $port            The keyless SSL port used to commmunicate between CloudFlare and the client's Keyless SSL server
      * @param string      $name            The keyless SSL name
