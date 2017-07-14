d585cef233f15c36c9f13f78e34793a0c4f9c170
Aleksei Faians <af.git@bsrealm.net>
2017-07-14 14:17:54 +0300

    add always_use_https method

--- src/CloudFlare/Zone/Settings.php
+++ src/CloudFlare/Zone/Settings.php
@@ -39,6 +39,17 @@ class Settings extends Api
     }
 
     /**
+     * Get Always Use HTTPS setting permission needed: #zone_settings:read
+     * Reply to all requests for URLs that use "http" with a 301 redirect to the equivalent "https" URL. If you only want to redirect for a subset of requests, consider creating an "Always use HTTPS" page rule.
+     *
+     * @param string $zone_identifier API item identifier tag
+     */
+    public function always_use_https($zone_identifier)
+    {
+        return $this->get('zones/'.$zone_identifier.'/settings/always_use_https');
+    }
+
+    /**
      * Get Always Online setting (permission needed: #zone_settings:read)
      * When enabled, Always Online will serve pages from our cache if your server is offline
      * (https://support.cloudflare.com/hc/en-us/articles/200168006)
@@ -383,6 +394,22 @@ class Settings extends Api
         ];
 
         return $this->patch('zones/'.$zone_identifier.'/settings', $data);
+    }
+
+    /**
+     * Change Always Use HTTPS setting permission needed: #zone_settings:edit
+     * Reply to all requests for URLs that use "http" with a 301 redirect to the equivalent "https" URL. If you only want to redirect for a subset of requests, consider creating an "Always use HTTPS" page rule.
+     *
+     * @param string      $zone_identifier API item identifier tag
+     * @param string|null $value           Value of the zone setting (default: off)
+     */
+    public function change_always_use_https($zone_identifier, $value = null)
+    {
+        $data = [
+            'value' => $value,
+        ];
+
+        return $this->patch('zones/'.$zone_identifier.'/settings/always_use_https', $data);
     }
 
     /**
